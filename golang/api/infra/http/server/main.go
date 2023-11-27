e549e23c64b194e065bc5427d1e6f2fdde95629d
Luiz Fantin <luiz.fantin.neto@gmail.com>
2023-11-27 09:21:07 -0300

    feat: remove env golang and create dockerfile

--- golang/api/infra/http/server/main.go
+++ golang/api/infra/http/server/main.go
@@ -2,12 +2,10 @@ package http
 
 import (
 	"log"
-	"os"
 
 	healthController "github.com/LuizFantin/tcc-benchmark/golang/api/infra/http/controllers/health-controller"
 	matrixController "github.com/LuizFantin/tcc-benchmark/golang/api/infra/http/controllers/matrix-controller"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func Init() {
@@ -18,13 +16,7 @@ func Init() {
 	router.GET("/health", healthController.Alive)
 	router.GET("/matrix", matrixController.Multiply)
 
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	port := os.Getenv("PORT")
+	port := "8000"
 
 	log.Println("Running server in port " + port)
 	router.Run(":" + port)
